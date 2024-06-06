198c5599ad63c44cc3d43765292e0f72ea432b4b
Jeremi <23479392+jeremia-oliver@users.noreply.github.com>
2024-06-06 21:21:26 +0700

    Update Playwright Test

--- tests/test.spec.ts
+++ tests/test.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect } from '@playwright/test';
 
-const URL = 'http://localhost:3000/'
-// const URL = 'https://poke-api-one-eta.vercel.app/'
+// const URL = 'http://localhost:3000/'
+const URL = 'https://poke-api-one-eta.vercel.app/'
 
 test.describe('Test Poke API Website', () => {
   test.beforeEach(async ({ page }) => {
@@ -9,13 +9,12 @@ test.describe('Test Poke API Website', () => {
   });
 
   test('Showing all pokemon image to be visible', async ({ page }) => {
-    const loader = await page.getByText('Loading...').isVisible();
-    if(!loader){
-      let images = page.locator("img")
-      await expect(images).toHaveCount(1025)
-      for (const image of await images.all()) {
-        await expect(image).toBeVisible();
-      }
+    test.setTimeout(150000)
+    await expect(page.getByText('Loading...')).toBeHidden({ timeout: 150000 })
+    let images = page.locator("img")
+    await expect(images).toHaveCount(1025)
+    for (const image of await images.all()) {
+      await expect(image).toBeVisible();
     }
   });
 });
@@ -26,28 +25,55 @@ test.describe('Test Search Function', () => {
   });
 
   test('Showing pokemon based on search value', async ({ page }) => {
-    test.setTimeout(120000)
-    const loader = await page.getByText('Loading...').isVisible();
-    if(!loader){
-      let searchInput = await page.getByPlaceholder("Search by name").fill('saur')
-      let images = page.locator("img")
-      for (const image of await images.all()) {
-        await expect(image).toBeVisible();
-      }
-    }
+    await expect(page.getByText('Loading...')).toBeHidden({ timeout: 150000 })
+    let searchInput = await page.getByPlaceholder("Search by name").fill('saur')
+    await expect(page.getByText('saur')).toHaveCount(3)
   });
 
   test('Showing not found if no pokemon match search value', async ({ page }) => {
-    test.setTimeout(120000)
-    const loader = await page.getByText('Loading...').isVisible();
-    if(!loader){
-      let searchInput = await page.getByPlaceholder("Search by name").fill('notmatch')
-      let images = page.locator("img")
-      await expect(page.getByText("Pokemon Not Found")).toBeVisible()
-      for (const image of await images.all()) {
-        await expect(image).toBeHidden();
-      }
-    }
+    await expect(page.getByText('Loading...')).toBeHidden({ timeout: 150000 })
+    let searchInput = await page.getByPlaceholder("Search by name").fill('notmatch')
+    await expect(page.getByText('notmatch')).toHaveCount(0)
+  });
+
+});
+
+test.describe('Test Filter Function', () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto(URL);
+  });
+
+  test('Showing pokemon based on type filter', async ({ page }) => {
+    test.setTimeout(150000)
+    await expect(page.getByText('Loading...')).toBeHidden()
+    await page.getByRole('button', { name: 'Filter' }).click();
+    await page.getByPlaceholder('Select a type').click();
+    await page.getByRole('option', { name: 'NORMAL' }).click();
+    await page.getByRole('button', { name: 'Filter' }).click();
+    await expect(page.getByText('Loading...')).toBeHidden({ timeout: 150000 })
+    await expect(page.getByText("Displaying 131 Pokemon")).toBeVisible()
+  });
+
+  test('Showing pokemon based on abilty filter', async ({ page }) => {
+    test.setTimeout(150000)
+    await expect(page.getByText('Loading...')).toBeHidden()
+    await page.getByRole('button', { name: 'Filter' }).click();
+    await page.getByPlaceholder('Select an ability').click();
+    await page.getByRole('option', { name: 'LIMBER', exact: true  }).click();
+    await page.getByRole('button', { name: 'Filter' }).click();
+    await expect(page.getByText('Loading...')).toBeHidden({ timeout: 150000 })
+    await expect(page.getByText("Displaying 14 Pokemon")).toBeVisible()
+  });
+
+  test('Showing pokemon based on move filter', async ({ page }) => {
+    test.setTimeout(150000)
+    await expect(page.getByText('Loading...')).toBeHidden()
+    await page.getByRole('button', { name: 'Filter' }).click();
+    await page.getByPlaceholder('Select a move').click();
+    await page.getByRole('option', { name: 'KARATE CHOP', exact: true  }).click();
+    await page.getByRole('button', { name: 'Filter' }).click();
+    await expect(page.getByText('Loading...')).toBeHidden({ timeout: 150000 })
+    await expect(page.getByText("Displaying 11 Pokemon")).toBeVisible()
   });
 
 });
