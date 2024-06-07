d496fedd350183f6bf8517b8ff3caa34529aedbf
Jeremi <23479392+jeremia-oliver@users.noreply.github.com>
2024-06-07 07:18:49 +0700

    Update Test Script

--- tests/test.spec.ts
+++ tests/test.spec.ts
@@ -26,14 +26,8 @@ test.describe('Test Search Function', () => {
 
   test('Showing pokemon based on search value', async ({ page }) => {
     await expect(page.getByText('Loading...')).toBeHidden({ timeout: 150000 })
-    let searchInput = await page.getByPlaceholder("Search by name").fill('saur')
+    await page.getByPlaceholder("Search by name").fill('saur')
     await expect(page.getByText('saur')).toHaveCount(3)
-  });
-
-  test('Showing not found if no pokemon match search value', async ({ page }) => {
-    await expect(page.getByText('Loading...')).toBeHidden({ timeout: 150000 })
-    let searchInput = await page.getByPlaceholder("Search by name").fill('notmatch')
-    await expect(page.getByText('notmatch')).toHaveCount(0)
   });
 
 });
@@ -74,6 +68,35 @@ test.describe('Test Filter Function', () => {
     await page.getByRole('button', { name: 'Filter' }).click();
     await expect(page.getByText('Loading...')).toBeHidden({ timeout: 150000 })
     await expect(page.getByText("Displaying 11 Pokemon")).toBeVisible()
+  });
+
+});
+
+test.describe('Test Failed Search and Filter Function', () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto(URL);
+  });
+  
+  test('Showing not found if no pokemon match search value', async ({ page }) => {
+    await expect(page.getByText('Loading...')).toBeHidden({ timeout: 150000 })
+    await page.getByPlaceholder("Search by name").fill('notmatch')
+    await expect(page.getByText('notmatch')).toHaveCount(0)
+    await expect(page.getByText("Pokemon Not Found")).toHaveCount(1)
+  });
+  
+  test('Showing not found if no pokemon match filter value', async ({ page }) => {
+    await expect(page.getByText('Loading...')).toBeHidden({ timeout: 150000 })
+    
+    await page.getByRole('button', { name: 'Filter' }).click();
+    await page.getByPlaceholder('Select a type').click();
+    await page.getByRole('option', { name: 'GROUND' }).click();
+    await page.getByPlaceholder('Select an ability').click();
+    await page.getByRole('option', { name: 'STENCH', exact: true  }).click();
+    await page.getByPlaceholder('Select a move').click();
+    await page.getByRole('option', { name: 'FLY', exact: true  }).click();
+    await page.getByRole('button', { name: 'Filter' }).click();
+    await expect(page.getByText('Loading...')).toBeHidden({ timeout: 150000 })
+    await expect(page.getByText("Pokemon Not Found")).toHaveCount(1)
   });
 
 });
